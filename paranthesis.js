734dccf13e074b950bf9ab85b09a5b2681d23a05
Aaron Williams <aaronrw4@yahoo.com>
2021-08-27 06:33:24 -0700

    first commit

---
+++ paranthesis.js
@@ -0,0 +1,42 @@
+var longestValidParentheses = function(s) {
+    let result = 0
+
+    for (i = 0; i < s.length; i++) {
+        let pointer = i
+        let openParenthesis = 0
+        let parenthesisArr = []
+        let substringLength = 0
+
+        if (s[i] === '(') {
+            parenthesisArr.push('(')
+            openParenthesis++
+            pointer++  
+
+            while (pointer < s.length) {
+                if (s[pointer] === '(') {
+                    parenthesisArr.push('(')
+                    openParenthesis++
+                    pointer++
+                } else if (s[pointer] === ')' && openParenthesis === 1) {
+                    substringLength = substringLength + 2 * parenthesisArr.length
+                    parenthesisArr = []
+                    openParenthesis--
+                    pointer++
+                } else if (s[pointer] === ')' && openParenthesis > 1) {
+                    openParenthesis--
+                    pointer++
+                } else if (s[pointer] === ')' && openParenthesis === 0) {
+                    break
+                }                
+            }
+        }
+
+        if (substringLength > result) {
+            result = substringLength
+        }
+    }
+
+    return result
+};
+
+console.log(longestValidParentheses(")()())()()("))
